7c42f5c498d021987b4c45894d71caef9abfe8b8
kuwaharu <141981256+kuwaharu-git@users.noreply.github.com>
2025-08-30 13:21:41 +0900

    Initial Commit

---
+++ TouchTheNumber/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TouchTheNumber
+//
+//  Created by 桑島陽斗 on 2025/08/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TouchTheNumber/TouchTheNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchTheNumberApp.swift
+//  TouchTheNumber
+//
+//  Created by 桑島陽斗 on 2025/08/30.
+//
+
+import SwiftUI
+
+@main
+struct TouchTheNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
